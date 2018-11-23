f1c5faa229171d8f33e03412c35f22a4ad9d061c
Jones Chakkalakkal <joneschakk@gmail.com>
2018-11-23 15:51:23 +0530

    'html'

--- smp_http.py
+++ smp_http.py
@@ -7,11 +7,17 @@ class reqhand(BaseHTTPServer.BaseHTTPRequestHandler):
     nodes={}
 
     def do_GET(self):
-        print 'Hello GET'
-        print 'Reqhand is ',reqhand.nodes
+        #print 'Hello GET'
+        print 'Node Status is ',reqhand.nodes
         for value in self.nodes.itervalues():
             if value[2]==True:
                 print 'Master',value
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        self.wfile.write('Master is '+value[0]+':'+str(value[1]))
+        return
 
 
     
