1841e90d55512b92fe5a35f3724d0987fee302f7
joneschakk <joneschakk@gmail.com>
2018-11-23 16:12:53 +0530

    Update test_launch.sh

--- test_launch.sh
+++ test_launch.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gnome-terminal -x zsh -c "python core.py 8120 3131;exec zsh";
-gnome-terminal -x zsh -c "python core.py 8121 3132;exec zsh";
-gnome-terminal -x zsh -c "python core.py 8122 3133;exec zsh";
+gnome-terminal -x bash -c "python2 core.py 8120 3131;exec zsh";
+gnome-terminal -x bash -c "python2 core.py 8121 3132;exec zsh";
+gnome-terminal -x bash -c "python2 core.py 8122 3133;exec zsh";
